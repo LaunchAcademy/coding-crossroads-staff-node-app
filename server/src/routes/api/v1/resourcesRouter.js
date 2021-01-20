8ed5fc8876da992400a42517c1ad8146c1db5e3c
dovakeen118 <kegillis118@gmail.com>
2021-01-20 17:57:32 -0500

    resource serializer

--- server/src/routes/api/v1/resourcesRouter.js
+++ server/src/routes/api/v1/resourcesRouter.js
@@ -1,18 +1,24 @@
 import express from "express";
 
 import { Resource } from "../../../models/index.js"
+import ResourceSerializer from "../../../serializers/ResourceSerializer.js"
 
 const resourcesRouter = new express.Router();
 
 resourcesRouter.get("/", async (req, res) => {
   const resources = await Resource.query()
-  return res.status(200).json({ resources })
+  const serializedResources = resources.map((resource) => {
+    return ResourceSerializer.getSummary(resource)
+  })
+
+  return res.status(200).json({ resources: serializedResources })
 })
 
 resourcesRouter.get("/:id", async (req,res) => {
   try {
     const resource = await Resource.query().findById(req.params.id).throwIfNotFound()
-    return res.status(200).json({ resource })
+    const serializedResource = ResourceSerializer.getDetails(resource)
+    return res.status(200).json({ resource: serializedResource })
   } catch (err) {
     return res.status(500).json({ errors: err })
   }
