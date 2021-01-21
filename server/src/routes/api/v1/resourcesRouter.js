c9257aa0a6f44bb732cd503c4e9135f08a8ebcaa
dovakeen118 <kegillis118@gmail.com>
2021-01-20 19:57:04 -0500

    singular serializer

--- server/src/routes/api/v1/resourcesRouter.js
+++ server/src/routes/api/v1/resourcesRouter.js
@@ -7,7 +7,7 @@ const resourcesRouter = new express.Router();
 
 resourcesRouter.get("/", async (req, res) => {
   const resources = await Resource.query()
-  const serializedResources = resources.map(resource => ResourcesSerializer.getSummary(resource))
+  const serializedResources = resources.map(resource => ResourceSerializer.getSummary(resource))
 
   return res.status(200).json({ resources: serializedResources })
 })
